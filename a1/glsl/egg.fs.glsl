e43dc1f27991a1a3ad2ebbf6f9313dec41ae78b4
Luciano Burla <silverbu@cs.ubc.ca>
2019-01-06 17:19:39 -0800

    Initial Commit

---
+++ a1/glsl/egg.fs.glsl
@@ -0,0 +1,16 @@
+#version 300 es
+
+precision highp float;
+precision highp int;
+out vec4 out_FragColor; 
+
+// HINT: YOU WILL NEED TO PASS IN THE CORRECT VARYING (SHARED) VARIABLE)
+
+void main() {
+
+  // HINT: YOU WILL NEED TO SET YOUR OWN DISTANCE THRESHOLD
+
+  // Set constant color red
+  out_FragColor = vec4(1.0, 0.0, 0.0, 1.0); // REPLACE ME
+
+}
